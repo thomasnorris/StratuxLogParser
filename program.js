163c9f05102cf82cdc7e62d6b5d8c4181a3612d6
Thomas Norris <tom.norris135@gmail.com>
2020-01-29 14:16:30 -0500

    reads files; added methods for logs and csv files

--- program.js
+++ program.js
@@ -15,19 +15,51 @@ const SQL_CFG = {
 }
 
 _sql.connect(SQL_CFG, (err) => {
-    if (err) {
-        console.log(err);
-        process.exit(0);
-    } else {
-        console.log('\n\nConnection successful.\n\n');
-        readFile();
-    }
+    if (err)
+        halt(err);
+
+    console.log('\n\nConnection successful.\n\n');
+    readFile();
 });
 
 function readFile() {
-    _rl.question('File to read (no validation): ', (fileName) => {
+    _rl.question('File path to read with extension [no validation][.log, .csv, .txt]: ', (fileName) => {
+        _rl.close();
 
+        // grab extension
+        var regex = /\.[0-9a-z]+$/i;
+        var ext = fileName.match(regex)[0].toLowerCase();
+
+        // insert into the file info table and continue
+        var query = 'INSERT INTO dbo.FileInfo (file_name, date_parsed) OUTPUT Inserted.ID VALUES (\'' + fileName + '\', GETDATE())';
+        sendRequest(query, (res) => {
+            var fileInfoID = res.recordset[0].ID;
+            if (ext === '.csv')
+                parseCSV(name, fileInfoID);
+            else
+                parseLog(name, fileInfoID);
+        });
     });
 }
 
+function parseCSV(name, fileInfoID) {
 
+}
+
+function parseLog(name, fileInfoID) {
+
+}
+
+function sendRequest(query, cb) {
+    var req = new _sql.Request();
+    req.query(query, (err, res) => {
+        if (err)
+            halt(err);
+        cb(res);
+    })
+}
+
+function halt(err) {
+    console.log(err);
+    process.exit(0);
+}
