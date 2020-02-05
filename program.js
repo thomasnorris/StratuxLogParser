f3ba3fedb0e34afdde2d63099b5c075554440316
Thomas Norris <tom.norris135@gmail.com>
2020-02-05 14:40:16 -0500

    simplification with promises

--- program.js
+++ program.js
@@ -20,15 +20,29 @@ const LOG_MATCH_REGEX = /stratux.log/g;
 // const LOG_DIR = '/var/log/'
 const LOG_DIR = 'C:/Users/tnorris/Desktop/';
 
-_sql.connect(SQL_CFG, (err) => {
-    if (err)
-        halt(err);
 
-    console.log('\n\nConnection successful.\n');
-    afterSqlConnect();
-});
+(async function() {
+    await dbConnect();
 
-async function afterSqlConnect() {
+    var file = await loadFile();
+    var results = await parseFile(file);
+
+    exit(results.table + ' updated with ' + results.count + ' new records.\nFile info ID: ' + file.infoID);
+})();
+
+async function dbConnect() {
+    return new Promise((resolve, reject) => {
+        _sql.connect(SQL_CFG, (err) => {
+            if (err)
+                exit(err);
+
+            console.log('\n\nConnection successful.\n');
+            resolve();
+        });
+    })
+}
+
+async function loadFile() {
     var rl = _rl.createInterface({
         input: process.stdin,
         output: process.stdout
@@ -36,30 +50,31 @@ async function afterSqlConnect() {
 
     var files = await listMatchingFiles();
 
-    rl.question('\nEnter file number to parse [0 - ' + (files.length - 1) + ']: ', (index) => {
-        rl.close();
+    return new Promise((resolve, reject) => {
+        rl.question('\nEnter file number to parse [0 - ' + (files.length - 1) + ']: ', (index) => {
+            rl.close();
 
-        if (!files[index])
-            halt('Undefined file.');
+            if (!files[index])
+                exit('Undefined file.');
 
-        var fileName = files[index];
-        var sql = 'INSERT INTO ' + FILE_INFO_TABLE + ' (file_name, date_parsed) OUTPUT Inserted.ID VALUES (\'' + fileName + '\', GETDATE())';
-        sendRequest(sql, (res) => {
-            console.log('\nParsing \'' +  fileName + '\', please wait...');
-
-            var filePath = LOG_DIR + fileName;
-            var fileInfoID = res.recordset[0].ID;
-            if (fileName.match( /\.[0-9a-z]+$/g)[0].toLowerCase() === '.csv')
-                parseCSV(filePath, fileInfoID);
-            else
-                parseLog(filePath, fileInfoID);
+            var fileName = files[index];
+            var sql = 'INSERT INTO ' + FILE_INFO_TABLE + ' (file_name, date_parsed) OUTPUT Inserted.ID VALUES (\'' + fileName + '\', GETDATE())';
+            sendRequest(sql, (res) => {
+                resolve({
+                    name: fileName,
+                    fullPath: LOG_DIR + fileName,
+                    infoID: res.recordset[0].ID,
+                    extension: fileName.match( /\.[0-9a-z]+$/g)[0].toLowerCase()
+                });
+            });
         });
     });
 
     async function listMatchingFiles() {
-        return new Promise((res, rej) => {
+        return new Promise((resolve, reject) => {
             _fs.readdir(LOG_DIR, (err, files) => {
                 console.log('Matching files in \'' + LOG_DIR + '\':');
+
                 var filtered = [];
                 var filterCount = 0;
                 files.filter((file) => {
@@ -68,87 +83,101 @@ async function afterSqlConnect() {
                         filtered.push(file);
                     }
                 });
-                res(filtered);
+
+                resolve(filtered);
             });
         });
     }
 }
 
+async function parseFile(file) {
+    console.log('\nParsing \'' +  file.name + '\', please wait...');
+    return new Promise((resolve, reject) => {
+        if (file.extension === '.csv')
+            parseCSV(resolve, reject);
+        else
+            parseLog(resolve, reject);
+    });
 
-function parseCSV(filePath, fileInfoID) {
-    var sql;
-    var rows;
-    var columns;
-    var readCount = 0;
-    var sentCount = 0;
-    _fs.createReadStream(filePath)
-        .pipe(_csv())
-        .on('data', (data) => {
-            ++readCount;
-            data.file_info_ID = fileInfoID;
-            var keys = Object.keys(data);
+    function parseCSV(resolve, reject) {
+        var sql;
+        var rows;
+        var columns;
+        var readCount = 0;
+        var sentCount = 0;
 
-            // build and send each query
-            sql = 'INSERT INTO ' + CSV_TABLE + ' (';
-            rows = '';
-            columns = '';
-            keys.forEach((key) => {
-                rows += key
-                columns += data[key];
+        _fs.createReadStream(file.fullPath)
+            .pipe(_csv())
+            .on('data', (data) => {
+                ++readCount;
+                data.file_info_ID = file.infoID;
+                sql = 'INSERT INTO ' + CSV_TABLE + ' (';
+                rows = '';
+                columns = '';
 
-                // don't add a comma on the last read key
-                if (keys.indexOf(key) !== keys.length - 1) {
-                    rows += ', ';
-                    columns += ', ';
-                }
+                var keys = Object.keys(data);
+                keys.forEach((key) => {
+                    rows += key
+                    columns += data[key];
+
+                    // last key doesn't need a comma
+                    if (keys.indexOf(key) !== keys.length - 1) {
+                        rows += ', ';
+                        columns += ', ';
+                    }
+                });
+
+                sql += rows + ') VALUES (' + columns + ');';
+
+                sendRequest(sql, (res) => {
+                    ++sentCount;
+                    if (sentCount === readCount)
+                        resolve({
+                            table: CSV_TABLE,
+                            count: sentCount
+                        });
+                });
             });
+    }
 
-            sql += rows + ') VALUES (' + columns + ');';
+    function parseLog(resolve, reject) {
+        var readCount = 0;
+        var sentCount = 0;
+        var rl = _rl.createInterface({
+            input: _fs.createReadStream(file.fullPath)
+        });
 
+        rl.on('line', (line) => {
+            ++readCount;
+            var splitStr = line.split(' ');
+            var date = splitStr[0];
+            var time = splitStr[1];
+
+            line = splitStr.slice(2, splitStr.length).join(' ');
+
+            var sql = 'INSERT INTO ' + LOG_TABLE + ' (file_info_ID, date, time, message) VALUES (' + file.infoID + ', \'' + date + '\', \'' + time + '\', \'' + line + '\')';
             sendRequest(sql, (res) => {
                 ++sentCount;
-                // reading will finish before sending, only complete when both are done
-                if (sentCount === readCount)
-                    halt(CSV_TABLE + ' updated with ' + sentCount + ' new records.');
+                if (readCount === sentCount)
+                    resolve({
+                        table: LOG_TABLE,
+                        count: sentCount
+                    });
             });
-        })
-}
-
-function parseLog(filePath, fileInfoID) {
-    var readCount = 0;
-    var sentCount = 0;
-    var rl = _rl.createInterface({
-        input: _fs.createReadStream(filePath)
-    });
-
-    rl.on('line', (line) => {
-        ++readCount;
-        var splitStr = line.split(' ');
-        var date = splitStr[0];
-        var time = splitStr[1];
-
-        line = splitStr.slice(2, splitStr.length).join(' ');
-
-        var sql = 'INSERT INTO ' + LOG_TABLE + ' (file_info_ID, date, time, message) VALUES (' + fileInfoID + ', \'' + date + '\', \'' + time + '\', \'' + line + '\')';
-        sendRequest(sql, (res) => {
-            ++sentCount;
-            // reading will finish before sending, only complete when both are done
-            if (readCount === sentCount)
-                halt(LOG_TABLE + ' updated with ' + sentCount + ' new records.')
         });
-    });
+    }
 }
 
 function sendRequest(sql, cb) {
     var req = new _sql.Request();
     req.query(sql, (err, res) => {
         if (err)
-            halt(err);
+            exit(err);
         cb(res);
-    })
+    });
 }
 
-function halt(msg) {
+function exit(msg) {
     console.log('\n' + msg + '\nExiting.');
     process.exit(0);
 }
