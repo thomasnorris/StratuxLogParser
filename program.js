7742103778cb2614394a45f4603a4f71986c9e46
Thomas Norris <tom.norris135@gmail.com>
2020-02-05 12:45:49 -0500

    now finds matching files and parses based on an index

--- program.js
+++ program.js
@@ -12,53 +12,74 @@ const SQL_CFG = {
     port: 1433
 }
 
-const CSV_TABLE = 'dbo.SensorCSVData';
-const LOG_TABLE = 'dbo.StratuxLogData';
+const CSV_TABLE = 'dbo.SensorDataCSV';
+const LOG_TABLE = 'dbo.StratuxDataLog';
 const FILE_INFO_TABLE = 'dbo.ParsedFileInfo';
+const CSV_MATCH_REGEX = /sensors.+.csv/g;
+const LOG_MATCH_REGEX = /stratux.log/g;
+// const LOG_DIR = '/var/log/'
+const LOG_DIR = 'C:/Users/tnorris/Desktop/';
 
 _sql.connect(SQL_CFG, (err) => {
     if (err)
         halt(err);
 
     console.log('\n\nConnection successful.\n');
-    readFile();
+    afterSqlConnect();
 });
 
-function readFile() {
+async function afterSqlConnect() {
     var rl = _rl.createInterface({
         input: process.stdin,
         output: process.stdout
     });
 
-    var fileName = 'stratux.log'
+    var files = await listMatchingFiles();
 
-    rl.question('File path to read with extension [no validation][.log, .txt, .csv]: ', (fileName) => {
+    rl.question('\nEnter file number to parse [0 - ' + (files.length - 1) + ']: ', (index) => {
         rl.close();
 
-        // grab extension
-        var regex = /\.[0-9a-z]+$/i;
-        var ext = fileName.match(regex)[0].toLowerCase();
+        if (!files[index])
+            halt('Undefined file.');
 
-        // insert into FILE_INFO_TABLE table and send the ID alone
+        var fileName = files[index];
         var sql = 'INSERT INTO ' + FILE_INFO_TABLE + ' (file_name, date_parsed) OUTPUT Inserted.ID VALUES (\'' + fileName + '\', GETDATE())';
         sendRequest(sql, (res) => {
-            console.log('Parsing \'' + fileName + '\', please wait...');
+            console.log('\nParsing \'' +  filePath + '\', please wait...');
+            var filePath = LOG_DIR + fileName;
             var fileInfoID = res.recordset[0].ID;
-            if (ext === '.csv')
-                parseCSV(fileName, fileInfoID);
+            if (fileName.match( /\.[0-9a-z]+$/g)[0].toLowerCase() === '.csv')
+                parseCSV(filePath, fileInfoID);
             else
-                parseLog(fileName, fileInfoID);
+                parseLog(filePath, fileInfoID);
         });
     });
 }
 
-function parseCSV(name, fileInfoID) {
+async function listMatchingFiles() {
+    return new Promise((res, rej) => {
+        _fs.readdir(LOG_DIR, (err, files) => {
+            console.log('Matching files in \'' + LOG_DIR + '\':');
+            var filtered = [];
+            var filterCount = 0;
+            files.filter((file) => {
+                if (file.match(CSV_MATCH_REGEX) || file.match(LOG_MATCH_REGEX)) {
+                    console.log(filterCount++ + ': ' + file);
+                    filtered.push(file);
+                }
+            });
+            res(filtered);
+        });
+    });
+}
+
+function parseCSV(filePath, fileInfoID) {
     var sql;
     var rows;
     var columns;
     var readCount = 0;
     var sentCount = 0;
-    _fs.createReadStream(name)
+    _fs.createReadStream(filePath)
         .pipe(_csv())
         .on('data', (data) => {
             ++readCount;
@@ -91,11 +112,11 @@ function parseCSV(name, fileInfoID) {
         })
 }
 
-function parseLog(name, fileInfoID) {
+function parseLog(filePath, fileInfoID) {
     var readCount = 0;
     var sentCount = 0;
     var rl = _rl.createInterface({
-        input: _fs.createReadStream(name)
+        input: _fs.createReadStream(filePath)
     });
 
     rl.on('line', (line) => {
@@ -106,7 +127,7 @@ function parseLog(name, fileInfoID) {
 
         line = splitStr.slice(2, splitStr.length).join(' ');
 
-        var sql = 'INSERT INTO ' + LOG_TABLE + ' (file_info_ID, date, time, data_string) VALUES (' + fileInfoID + ', \'' + date + '\', \'' + time + '\', \'' + line + '\')';
+        var sql = 'INSERT INTO ' + LOG_TABLE + ' (file_info_ID, date, time, message) VALUES (' + fileInfoID + ', \'' + date + '\', \'' + time + '\', \'' + line + '\')';
         sendRequest(sql, (res) => {
             ++sentCount;
             // reading will finish before sending, only complete when both are done
@@ -126,6 +147,6 @@ function sendRequest(sql, cb) {
 }
 
 function halt(msg) {
-    console.log(msg + '\nExiting.');
+    console.log('\n' + msg + '\nExiting.');
     process.exit(0);
 }
